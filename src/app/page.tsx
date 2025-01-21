f2e1e42774edc2c2e1fb3c4183794d21e451fcb1
OmarZaatari <106358964+OmarZaatari@users.noreply.github.com>
2025-01-21 16:43:51 +0200

    feat: added Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero, ProductBuilding, Stats } from "@/components";
+import { Hero, ProductBuilding, Stats, Footer } from "@/components";
 
 export default function Home() {
     return (
@@ -6,6 +6,7 @@ export default function Home() {
             <Hero />
             <Stats />
             <ProductBuilding />
+            <Footer />
         </div>
     );
 }
