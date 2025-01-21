e31ac1e19333b9da5a4f6f51f4af3c28e567d689
OmarZaatari <106358964+OmarZaatari@users.noreply.github.com>
2025-01-21 16:43:13 +0200

    feat: Added Footer export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Hero } from "./hero";
-export { default as ProductBuilding } from "./product-building";    
+export { default as ProductBuilding } from "./product-building";
 export { default as Stats } from "./stats";
+export { default as Footer } from "./footer";
