423b08e4bee404bca36c26ea2b264d841b3ec89b
Mohamad Chebli <mchebli008@hotmail.com>
2025-01-27 11:51:33 +0200

    feat: add hero section

---
+++ src/components/sections/hero.tsx
@@ -0,0 +1,20 @@
+export default function Hero() {
+    return (
+        <div className="md:px-8">
+            <div className="bg-hero h-full min-h-[70vh] w-full bg-no-repeat bg-cover bg-center px-5 md:px-10 py-5 flex flex-col items-start justify-between">
+                <h1 className="text-3xl md:text-7xl text-white font-bold">
+                    Digital Experiences <br /> That Drive <br /> Results
+                </h1>
+                <div>
+                    <p className="md:font-semibold text-white text-xl">
+                        In a world where every click matters,
+                    </p>
+                    <p className="md:font-semibold text-white text-xl">
+                        we craft solutions that connect, convert, and create
+                        lasting impact.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+}
